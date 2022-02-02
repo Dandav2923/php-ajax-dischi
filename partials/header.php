215c30c5558d6bd493ed935e4ace8cac2850f343
Dandav2923 <daniele.emili23@gmail.com>
2022-02-02 15:06:05 +0100

    img

---
+++ partials/header.php
@@ -0,0 +1,10 @@
+<div class="container-fluid">
+    <div class="row bg-black">
+      <div class="col-1">
+        <div class="container-img">
+          <img class="w-50" src="img/logo.png" alt="">
+        </div>
+      </div>
+    </div>
+  </div>
+
